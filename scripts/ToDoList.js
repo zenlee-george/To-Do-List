18c5d008295eb74dbb99351ca6ace2d6aed6ebaf
zenlee-george <zenlee.george1@gmail.com>
2024-06-07 14:07:09 +0200

    removed duplicate event listener

--- scripts/ToDoList.js
+++ scripts/ToDoList.js
@@ -19,18 +19,6 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
-    addBtn.addEventListener("click", function() {
-        const newItem = addItemInput.value.trim();
-        if (newItem!== "" && newItem.length > 3 && newItem.charAt(0) === newItem.charAt(0).toUpperCase()) {
-            toDoList.push(newItem);
-            addItemInput.value = "";
-            renderToDoList();
-            errorMessageDiv.textContent = "";
-        } else {
-            errorMessageDiv.textContent = "Invalid input. Please check the constraints.";
-        }
-    });
-    
     sortBtn.addEventListener("click", function() {
         toDoList.sort();
         renderToDoList();
