f4bd51be8cd46e9e9d5d4f9d10da577f4206448a
zenlee-george <zenlee.george1@gmail.com>
2024-06-07 13:33:41 +0200

    added functionality to input field

--- scripts/ToDoList.js
+++ scripts/ToDoList.js
@@ -1,7 +1,8 @@
 let toDoList = [];
 
 document.addEventListener("DOMContentLoaded", function() {
-    const addBtn = document.getElementById("add-btn"); 
+    const addBtn = document.getElementById("add-btn");
+    const addItemInput = document.getElementById("new-item");
 
     addBtn.addEventListener("click", function() {
         const newItem = addItemInput.value.trim();
@@ -14,6 +15,19 @@ document.addEventListener("DOMContentLoaded", function() {
             errorMessageDiv.textContent = "Invalid input. Please check the constraints.";
         }
     });
+
+    addBtn.addEventListener("click", function() {
+        const newItem = addItemInput.value.trim();
+        if (newItem!== "" && newItem.length > 3 && newItem.charAt(0) === newItem.charAt(0).toUpperCase()) {
+            toDoList.push(newItem);
+            addItemInput.value = "";
+            renderToDoList();
+            errorMessageDiv.textContent = "";
+        } else {
+            errorMessageDiv.textContent = "Invalid input. Please check the constraints.";
+        }
+    });
+
     function renderToDoList() {
         toDoListUL.innerHTML = "";
         toDoList.forEach(function(item) {
